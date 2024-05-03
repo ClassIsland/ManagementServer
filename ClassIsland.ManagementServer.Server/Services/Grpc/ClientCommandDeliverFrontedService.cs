d18dbaf90fc767e8f5ca4ff4cb410199ef7a7b9b
WRC <hello_wrc@outlook.com>
2024-05-03 12:36:16 +0800

    pref: 命令流 Ping 改进

--- ClassIsland.ManagementServer.Server/Services/Grpc/ClientCommandDeliverFrontedService.cs
+++ ClassIsland.ManagementServer.Server/Services/Grpc/ClientCommandDeliverFrontedService.cs
@@ -1,9 +1,11 @@
 using ClassIsland.Core.Protobuf.Client;
+using ClassIsland.Core.Protobuf.Command;
 using ClassIsland.Core.Protobuf.Enum;
 using ClassIsland.Core.Protobuf.Server;
 using ClassIsland.Core.Protobuf.Service;
 using ClassIsland.ManagementServer.Server.Entities;
 using ClassIsland.ManagementServer.Server.Enums;
+using Google.Protobuf;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 
@@ -35,7 +37,35 @@ public class ClientCommandDeliverFrontedService(ClientCommandDeliverService clie
                 AssigneeType = (int)AssigneeTypes.ClientUid,
                 TargetClientCuid = cuid
             });
-        await Task.Run(() => context.CancellationToken.WaitHandle.WaitOne());
+        while (!context.CancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                await requestStream.MoveNext(context.CancellationToken);
+                var request = requestStream.Current;
+                if (request.Type == CommandTypes.Ping)
+                {
+                    await responseStream.WriteAsync(new ClientCommandDeliverScRsp()
+                    {
+                        RetCode = Retcode.Success,
+                        Type = CommandTypes.Pong,
+                        Payload = new HeartBeat().ToByteString()
+                    });
+                    Logger.LogTrace("Ping from {}", cuid);
+                }
+                else
+                {
+                    await responseStream.WriteAsync(new ClientCommandDeliverScRsp()
+                    {
+                        RetCode = Retcode.InvalidRequest
+                    });
+                }
+            }
+            catch (Exception e)
+            { 
+                break;
+            }
+        }
         ClientCommandDeliverService.Streams.Remove(ClientCommandDeliverService.Streams.FirstOrDefault(x => x.Value == responseStream).Key);
         Logger.LogInformation("断开与 {} 命令流连接", cuid);
     }
