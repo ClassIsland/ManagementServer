bce5176c1de43fb797d906865f90df4841ffa915
WRC <hello_wrc@outlook.com>
2024-08-03 09:27:03 +0800

    feat: 引入 monaco

--- classisland.managementserver.client/src/views/ClientDetailsView.vue
+++ classisland.managementserver.client/src/views/ClientDetailsView.vue
@@ -3,6 +3,7 @@ import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import createClient from "openapi-fetch";
 import type {paths} from "@/schema";
+import MonacoEditor from "@/components/MonacoEditor/MonacoEditor.vue";
 
 const route = useRoute()
 
@@ -17,41 +18,46 @@ const detailsTab = ref(0)
 </script>
 
 <template>
-  <div class="ma-4">
-    <v-sheet elevation="4" class="pa-4 gap-y-3">
+  <div class="ma-4 d-flex flex-column">
+    <div class="">
+      <v-sheet elevation="4" class="pa-4 gap-y-3">
 
-      <span class="text-h6">{{ data?.['id'] }}</span>
-      <p>
-        <span class="font-weight-bold">CUID: </span>
-        <span>{{ data?.['cuid'] }}</span>
-      </p>
-      <div class="d-flex ga-2 flex-row">
-        <v-btn variant="text" color="primary" prepend-icon="mdi-refresh">刷新客户端</v-btn>
-        <v-btn variant="text" color="primary" prepend-icon="mdi-link-off">解除绑定</v-btn>
-        <v-btn variant="text" color="primary" prepend-icon="mdi-message-alert-outline">发送提醒</v-btn>
-      </div>
-    </v-sheet>
-
-    <v-tabs class="mt-4" color="primary" v-model="detailsTab">
-      <v-tab :value="0">详细信息</v-tab>
-      <v-tab :value="1">已分配</v-tab>
-      <v-tab :value="2">结果</v-tab>
-      <v-tab :value="3">实例</v-tab>
-    </v-tabs>
-    <v-tabs-window v-model="detailsTab">
-      <v-tabs-window-item
-          :value="0"
-      >
+        <span class="text-h6">{{ data?.['id'] }}</span>
         <p>
           <span class="font-weight-bold">CUID: </span>
           <span>{{ data?.['cuid'] }}</span>
         </p>
-        <p>
-          <span class="font-weight-bold">首次注册: </span>
-          <span>{{ data?.['registerTime'] }}</span>
-        </p>
-      </v-tabs-window-item>
-    </v-tabs-window>
+        <div class="d-flex ga-2 flex-row">
+          <v-btn variant="text" color="primary" prepend-icon="mdi-refresh">刷新客户端</v-btn>
+          <v-btn variant="text" color="primary" prepend-icon="mdi-link-off">解除绑定</v-btn>
+          <v-btn variant="text" color="primary" prepend-icon="mdi-message-alert-outline">发送提醒</v-btn>
+        </div>
+      </v-sheet>
+    </div>
+
+    <div class="flex-grow-1 d-flex flex-column">
+      <v-tabs class="mt-4" color="primary" v-model="detailsTab">
+        <v-tab :value="0">详细信息</v-tab>
+        <v-tab :value="1">已分配</v-tab>
+        <v-tab :value="2">结果</v-tab>
+        <v-tab :value="3">实例</v-tab>
+      </v-tabs>
+      <v-tabs-window v-model="detailsTab" class="flex-grow-1 align-stretch">
+        <v-tabs-window-item
+            :value="0"
+            class="flex-grow-1"
+        >
+          <p>
+            <span class="font-weight-bold">CUID: </span>
+            <span>{{ data?.['cuid'] }}</span>
+          </p>
+          <p>
+            <span class="font-weight-bold">首次注册：</span>
+            <span>{{ data?.['registerTime'] }}</span>
+          </p>
+        </v-tabs-window-item>
+      </v-tabs-window>
+    </div>
   </div>
 </template>
 
--- classisland.managementserver.client/vite.config.ts
+++ classisland.managementserver.client/vite.config.ts
@@ -59,5 +59,14 @@ export default defineConfig({
       key: fs.readFileSync(keyFilePath),
       cert: fs.readFileSync(certFilePath),
     }
-  }
+  },
+  optimizeDeps: {
+    include: [
+      `monaco-editor/esm/vs/language/json/json.worker`,
+      `monaco-editor/esm/vs/language/css/css.worker`,
+      `monaco-editor/esm/vs/language/html/html.worker`,
+      `monaco-editor/esm/vs/language/typescript/ts.worker`,
+      `monaco-editor/esm/vs/editor/editor.worker`
+    ],
+  },
 })
