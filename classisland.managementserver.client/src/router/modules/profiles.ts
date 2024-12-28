33506fa2586114b4e3fb1d2670f39a6443fe71a4
WRC <hello_wrc@outlook.com>
2024-12-28 10:50:03 +0800

    feat: 上传本地档案

--- classisland.managementserver.client/src/router/modules/profiles.ts
+++ classisland.managementserver.client/src/router/modules/profiles.ts
@@ -1,6 +1,6 @@
 import { RouteRecordRaw } from 'vue-router';
 import { Layout } from '@/router/constant';
-import { DashboardOutlined } from '@vicons/antd';
+import { FileOutlined, BookOutlined, TableOutlined, CalendarOutlined, UploadOutlined } from '@vicons/antd';
 import { renderIcon } from '@/utils/index';
 
 const routeName = 'profiles';
@@ -9,22 +9,48 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/profiles',
     name: routeName,
-    redirect: '/profiles/',
     component: Layout,
     meta: {
       title: '档案',
-      icon: renderIcon(DashboardOutlined),
-      permissions: ['profile_manage'],
+      icon: renderIcon(FileOutlined),
       sort: 1,
     },
     children: [
+      {
+        path: '/profiles/classplans',
+        name: `${routeName}_classPlans`,
+        meta: {
+          title: '课表',
+          icon: renderIcon(TableOutlined),
+        },
+        component: () => import('@/views/profiles/subjects/index.vue'),
+      },
+      {
+        path: '/profiles/timetables',
+        name: `${routeName}_timetables`,
+        meta: {
+          title: '时间表',
+          icon: renderIcon(CalendarOutlined),
+        },
+        component: () => import('@/views/profiles/subjects/index.vue'),
+      },
       {
         path: '/profiles/subjects',
         name: `${routeName}_subjects`,
         meta: {
           title: '科目',
+          icon: renderIcon(BookOutlined),
         },
         component: () => import('@/views/profiles/subjects/index.vue'),
+      },
+      {
+        path: '/profiles/upload',
+        name: `${routeName}_upload`,
+        meta: {
+          title: '上传',
+          icon: renderIcon(UploadOutlined),
+        },
+        component: () => import('@/views/profiles/upload/index.vue'),
       },
       
     ],
---
+++ classisland.managementserver.client/src/views/profiles/upload/index.vue
@@ -0,0 +1,111 @@
+<script setup lang="ts">
+import { ArchiveOutline as ArchiveIcon } from '@vicons/ionicons5'
+import { defineComponent, ref } from 'vue'
+import type { UploadFileInfo, UploadInst, UploadCustomRequestOptions } from 'naive-ui'
+import { useMessage } from 'naive-ui'
+import {Alova} from "@/utils/http/alova";
+import {on} from "@/utils/domUtils";
+
+const message = useMessage();
+const override = ref(false);
+const upload = ref<UploadInst | null>(null);
+const isLoading = ref(false);
+
+const customRequest = async ({
+                         file,
+                         data,
+                         headers,
+                         withCredentials,
+                         action,
+                         onFinish,
+                         onError,
+                         onProgress
+                       }: UploadCustomRequestOptions) => {
+  isLoading.value = true;
+  onProgress({ percent: 10 });
+  try{
+    await Alova.Post('/api/v1/profiles/upload', await file.file?.text(), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      params: {
+        replace: override.value,
+      }
+    });
+  } catch (e) {
+    onError();
+    return;
+  }
+  onProgress({ percent: 100 });
+  onFinish()
+  message.success('上传成功');
+  isLoading.value = false;
+  
+};
+
+function beforeUpload(data: {
+  file: UploadFileInfo
+  fileList: UploadFileInfo[]
+}) {
+  if (data.file.file?.type !== 'application/json') {
+    message.error('只能上传 Json 档案文件')
+    return false
+  }
+  
+  return true
+}
+
+async function handleSubmit(){
+  upload.value?.submit();
+}
+
+function handleUpdate(fileList: UploadFileInfo[]){
+  console.log(fileList);
+}
+
+</script>
+
+<template>
+  <n-card :bordered="false" title="上传档案">
+    将本地的 ClassIsland 档案导入到集控服务器中。
+  </n-card>
+  <n-card :bordered="false" class="proCard mt-4" content-class="">
+    <n-upload
+      multiple
+      ref="upload"
+      directory-dnd
+      :default-upload="false"
+      :max="1"
+      :custom-request="customRequest"
+      accept=".json"
+      action="/api/v1/profiles/upload"
+      @onsubmit="handleSubmit"
+      @on-update="handleUpdate"
+      @beforeUpload="beforeUpload"
+    >
+      <n-upload-dragger>
+        <div style="margin-bottom: 12px">
+          <n-icon size="48" :depth="3">
+            <ArchiveIcon />
+          </n-icon>
+        </div>
+        <n-text style="font-size: 16px">
+          点击或者拖动文件到该区域来上传
+        </n-text>
+        <n-p depth="3" style="margin: 8px 0 0 0">
+          支持上传 ClassIsland 的档案文件
+        </n-p>
+      </n-upload-dragger>
+    </n-upload>
+    <n-checkbox v-model:checked="override">
+      覆盖存在的项目
+    </n-checkbox>
+    <n-button @click="handleSubmit" :loading="isLoading" type="primary" >
+      上传
+    </n-button>
+  </n-card>
+</template>
+
+<style scoped lang="less">
+
+</style>
