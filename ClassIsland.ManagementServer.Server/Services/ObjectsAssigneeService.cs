4e99adf08b1e96891135a5be772d1ad4f753c282
WRC <hello_wrc@outlook.com>
2025-06-15 12:35:51 +0800

    feat: 实现全局实例组选择

--- ClassIsland.ManagementServer.Server/Services/ObjectsAssigneeService.cs
+++ ClassIsland.ManagementServer.Server/Services/ObjectsAssigneeService.cs
@@ -24,6 +24,11 @@ public class ObjectsAssigneeService(ManagementServerContext context)
 
     public async Task<List<Client>> GetAssignedClients(ObjectsAssignee assignee)
     {
+        if (assignee.TargetGroupId == ClientGroup.GlobalGroupId)
+        {
+            return await DbContext.Clients.Select(x => x)
+                .ToListAsync();
+        }
         return await DbContext.Clients.Where(x =>
                 ((assignee.AssigneeType == AssigneeTypes.Group && assignee.TargetGroupId == x.AbstractClient.GroupId) ||
                  (assignee.AssigneeType == AssigneeTypes.Id && assignee.TargetClientId == x.Id) ||
@@ -33,22 +38,24 @@ public class ObjectsAssigneeService(ManagementServerContext context)
 
     public async Task<List<ObjectsAssignee>> GetClientAssigningObjects(Client client, ObjectTypes type)
     {
-        return await DbContext.ObjectsAssignees.Where(x => 
-            x.ObjectType == type &&
-            ((x.AssigneeType == AssigneeTypes.ClientUid && x.TargetClientCuid == client.Cuid) ||
-             (x.AssigneeType == AssigneeTypes.Id && x.TargetClientId == client.Id) ||
-             (x.AssigneeType == AssigneeTypes.Group && x.TargetGroupId == client.AbstractClient.GroupId))).Select(x => x).ToListAsync();
+        return await BuildClientAssigneeQuery(client, type)
+            .Select(x => x)
+            .ToListAsync();
     }
 
     public async Task<List<ObjectsAssignee>> GetClientAssigningObjectsLeveled(Client client, ObjectTypes type)
     {
-        return await DbContext.ObjectsAssignees.Where(x => 
-            x.ObjectType == type &&
-            ((x.AssigneeType == AssigneeTypes.ClientUid && x.TargetClientCuid == client.Cuid) ||
-             (x.AssigneeType == AssigneeTypes.Id && x.TargetClientId == client.Id) ||
-             (x.AssigneeType == AssigneeTypes.Group && x.TargetGroupId == client.AbstractClient.GroupId)))
+        return await BuildClientAssigneeQuery(client, type)
             .OrderBy(x => (int)x.AssigneeType)
             .Select(x => x)
             .ToListAsync();
     }
+
+    private IQueryable<ObjectsAssignee> BuildClientAssigneeQuery(Client client, ObjectTypes type) =>
+        DbContext.ObjectsAssignees.Where(x =>
+            x.ObjectType == type &&
+            ((x.AssigneeType == AssigneeTypes.Group && x.TargetGroupId == ClientGroup.GlobalGroupId) ||
+             (x.AssigneeType == AssigneeTypes.ClientUid && x.TargetClientCuid == client.Cuid) ||
+             (x.AssigneeType == AssigneeTypes.Id && x.TargetClientId == client.Id) ||
+             (x.AssigneeType == AssigneeTypes.Group && x.TargetGroupId == client.AbstractClient.GroupId)));
 }
