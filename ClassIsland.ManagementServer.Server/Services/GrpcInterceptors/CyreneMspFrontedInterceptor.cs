8ef7e5365a90f3732f9dba92d8309251777dead3
wrc <hello_wrc@outlook.com>
2025-08-16 20:15:13 +0800

    feat: CyreneMsp 鉴权

--- ClassIsland.ManagementServer.Server/Services/GrpcInterceptors/CyreneMspFrontedInterceptor.cs
+++ ClassIsland.ManagementServer.Server/Services/GrpcInterceptors/CyreneMspFrontedInterceptor.cs
@@ -3,8 +3,11 @@ using Grpc.Core.Interceptors;
 
 namespace ClassIsland.ManagementServer.Server.Services.GrpcInterceptors;
 
-public class CyreneMspFrontedInterceptor(ILogger<CyreneMspFrontedInterceptor> logger) : Interceptor
+public class CyreneMspFrontedInterceptor(ILogger<CyreneMspFrontedInterceptor> logger,
+    CyreneMspConnectionService cyreneMspConnectionService) : Interceptor
 {
+    private CyreneMspConnectionService CyreneMspConnectionService { get; } = cyreneMspConnectionService;
+    
     private const string ProtocolName = "Cyrene_MSP";
     
     public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(
@@ -17,6 +20,15 @@ public class CyreneMspFrontedInterceptor(ILogger<CyreneMspFrontedInterceptor> lo
         {
             throw new RpcException(new Status(StatusCode.InvalidArgument,
                 $"客户端使用的协议 {clientProtocolName} 与服务端协议 {ProtocolName} 不符。"));
+        }
+
+        if (!context.Method.StartsWith("/ClassIsland.Shared.Protobuf.Service.Handshake/") &&
+            (!CyreneMspConnectionService.Sessions.TryGetValue(Guid.Parse(context.RequestHeaders.GetValue("cuid")!),
+                 out var session) ||
+             session?.SessionId != context.RequestHeaders.GetValue("session"))
+            )
+        {
+            throw new RpcException(new Status(StatusCode.PermissionDenied, "会话无效"));
         }
         
         return await continuation(request, context);
